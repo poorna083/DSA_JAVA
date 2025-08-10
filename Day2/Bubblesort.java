cd16f97186e72b2a97410bc948f21a87a825bcf1
poorna083 <dpoorna083@gmail.com>
2025-08-10 12:02:56 +0530

    Bubble Sorting using arrayList

---
+++ Day2/Bubblesort.java
@@ -0,0 +1,32 @@
+//Bubble Sorting using arrayList
+package Day2;
+import java.util.*;
+
+public class Bubblesort {
+    public void Bubblesort(List<Integer> nums, int size) {
+        int temp;
+        for (int i = 0; i < size - 1; i++) {
+            for (int j = 0; j < size - i - 1; j++) {
+                if (nums.get(j) > nums.get(j + 1)) {
+                    temp = nums.get(j);
+                    nums.set(j, nums.get(j + 1));
+                    nums.set(j + 1, temp);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        List<Integer> nums = new ArrayList<>();
+        System.out.println("Enter the size:");
+        int size = sc.nextInt();
+        System.out.println("Enter the elements to sort:");
+        for (int i = 0; i < size; i++) {
+            nums.add(sc.nextInt());
+        }
+        Bubblesort b = new Bubblesort();
+        b.Bubblesort(nums, size);
+        System.out.println("Sorted list: " + nums);
+    }
+}
